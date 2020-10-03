64536bc091cf119a9bec318c5535fa85a6d9a307
Mashal Maliyakkal <mashal@carestack.com>
2020-10-03 17:15:18 +0530

    finish UI

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict'
 
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+//let today = new Date();
+//let formatDate = today.toDateString();
+//let selectElement = document.getElementById('date');
+//selectElement.innerHTML = formatDate;
 
+let refnum = document.getElementById('iref_no');
+refnum.placeholder = "akjs";
