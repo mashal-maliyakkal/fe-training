5255ee91735909acb476d62970fbab8be4e0aee5
Mashal Maliyakkal <mashal@carestack.com>
2020-10-05 23:27:24 +0530

    change icons & styling

--- app.js
+++ app.js
@@ -2,18 +2,51 @@
 // Get the element with id="myDIV" (a div), then get all elements inside div with class="example"
 //var x = document.getElementById("myDIV").querySelectorAll(".example"); 
 
+function openSlider() {
+    document.getElementById("slider").style.width = "90%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeSlider() {
+    document.getElementById("slider").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+}
+
+
+
+let modal = document.getElementById("idetails");
+let plusButtons = document.querySelectorAll("div.plus-icon-box > i");
+
+for (let btn of plusButtons) {
+    console.log('yehu');
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+}
+
+document.getElementById("modal-close").onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 function submit() {
     console.clear();
     const s = "&nbsp;";
-    var spans = document.getElementsByTagName('span'),
+    let spans = document.getElementsByTagName('span'),
         obj = {};
-    var formIds = ['ipay-amt','ipay-date','i-city','ipay-type','iref-no'];
-    var alertMsg = ['Please enter a valid amount.', 'Invalid payment date.',
+    let formIds = ['ipay-amt', 'ipay-date', 'i-city', 'ipay-type', 'iref-no'];
+    let alertMsg = ['Please enter a valid amount.', 'Invalid payment date.',
         'Please check given location.', 'Select payment type.', 'Please provide a valid reference number.'];
-    var alertDialogue = "";
+    let alertDialogue = "";
     for (let i = 0; i < spans.length; i++) {
         let input = document.getElementById(formIds[i]).value;
-        console.log(i,input);
+        console.log(i, input);
 
         let isValid = false;
         switch (i) {
@@ -21,7 +54,7 @@ function submit() {
                 isValid = !isNaN(input) && input > 0;
                 break;
             case 1:
-                isValid = input.length  > 3;
+                isValid = input.length > 3;
                 break;
             case 4:
                 isValid = !isNaN(input) && input !== "" && input > 0;
@@ -40,35 +73,6 @@ function submit() {
     }
 
     if (alertDialogue.length > 2) {
-        alert("Invalid entries on :"+alertDialogue);
-    }
-}
-
-function openSlider() {
-    document.getElementById("slider").style.width = "90%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeSlider() {
-    document.getElementById("slider").style.width = "0";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
-}
-
-var modal = document.getElementById("idetails");
-var plusButtons = document.getElementsByTagName("img");
-
-for (let btn of plusButtons) {
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
-}
-
-document.getElementById("modal-close").onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        alert("Invalid entries on :" + alertDialogue);
     }
 }
