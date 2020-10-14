16ff6199c298e830928b2e577faec2f532762569
Mashal Maliyakkal <mashal@carestack.com>
2020-10-14 15:40:43 +0530

    Change code as per suggestions

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 // Get the element with id="myDIV" (a div), then get all elements inside div with class="example"
 //var x = document.getElementById("myDIV").querySelectorAll(".example"); 
 
+let setupNotDone = true; 
+
 function openSlider() {
     document.getElementById("slider").style.width = "90%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    if(setupNotDone){
+        setupModal();
+        setupNotDone = false;
+    }
 }
 
 function closeSlider() {
@@ -12,41 +18,37 @@ function closeSlider() {
     document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
 }
 
+function setupModal(){
+    let modal = document.getElementById("details-modal");
+    let plusButtons = document.querySelectorAll("div.plus-icon-box > i");
 
-
-let modal = document.getElementById("idetails");
-let plusButtons = document.querySelectorAll("div.plus-icon-box > i");
-
-for (let btn of plusButtons) {
-    console.log('yehu');
-    btn.onclick = function () {
-        modal.style.display = "block";
+    for (let btn of plusButtons) {
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
     }
-}
 
-document.getElementById("modal-close").onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
+    document.getElementById("modal-close").onclick = function () {
         modal.style.display = "none";
     }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
 
-
-function submit() {
-    console.clear();
-    const s = "&nbsp;";
-    let spans = document.getElementsByTagName('span'),
-        obj = {};
+function submit() {    
+    let requiredStarLabels = document.querySelectorAll("label > span");
     let formIds = ['ipay-amt', 'ipay-date', 'i-city', 'ipay-type', 'iref-no'];
     let alertMsg = ['Please enter a valid amount.', 'Invalid payment date.',
         'Please check given location.', 'Select payment type.', 'Please provide a valid reference number.'];
+
+    const s = "&nbsp;";
     let alertDialogue = "";
-    for (let i = 0; i < spans.length; i++) {
+    for (let i = 0; i < requiredStarLabels.length; i++) {
         let input = document.getElementById(formIds[i]).value;
-        console.log(i, input);
 
         let isValid = false;
         switch (i) {
@@ -65,9 +67,9 @@ function submit() {
         }
 
         if (isValid) {
-            spans[i].innerHTML = s;
+            requiredStarLabels[i].innerHTML = s;
         } else {
-            spans[i].innerHTML = "*";
+            requiredStarLabels[i].innerHTML = "*";
             alertDialogue += "\n    " + alertMsg[i];
         }
     }
